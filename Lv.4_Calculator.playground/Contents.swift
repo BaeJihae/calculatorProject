f18952d828cd648aa7456ca30790c6af19139dd2
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-08 20:48:59 +0900

    추상화를 위한 Calculator 클래스 변경 -> 그에 따른 출력값 삭제

--- Lv.4_Calculator.playground/Contents.swift
+++ Lv.4_Calculator.playground/Contents.swift
@@ -70,37 +70,10 @@ class ModulusOperation: AbstractOperation {
 
 // Calculator 클래스
 class Calculator {
-
-    var firstNumber: Int = 0
-    var secondNumber: Int = 0
     
-    init(firstNumber: Int, secondNumber: Int) {
-        self.firstNumber = firstNumber
-        self.secondNumber = secondNumber
-    }
-    
-    func calculate(_ op: String) -> Double {
-
+    func calculate(_ firstNumber: Int, _ secondNumber: Int) -> Double {
         // 추상화된 부모 클래스를 타입으로 가지는 변수
         var calculator: AbstractOperation = AbstractOperation()
-        
-        // 연산에 따른 자식 클래스의 인스턴스를 할당
-        switch op {
-        case "+" :
-            calculator = AddOperation()
-        case "-" :
-            calculator = SubtractOperation()
-        case "/" :
-            calculator = DivideOperation()
-        case "*" :
-            calculator = MultiplyOperation()
-        case "%" :
-            calculator = ModulusOperation()
-        default :
-            print("잘못된 연산입니다.")
-            return 0.0
-        }
-        
         return calculator.calculate(firstNumber, secondNumber)
     }
 }
@@ -108,34 +81,11 @@ class Calculator {
 // 사칙연산 출력 함수
 func resultPrint(_ calculator: Calculator) {
     
-    // calculator 변수를 활용한 사칙연산
-    var calculateAdd = calculator.calculate("+")
-    var calculateSubstract = calculator.calculate("-")
-    var calculateDivide = calculator.calculate("/")
-    var calculateMutiply = calculator.calculate("*")
-    var calculateModulus = calculator.calculate("%")
 
-    // 사칙연산 출력
-    print("\(calculator.firstNumber)와 \(calculator.secondNumber)의 사칙연산")
-    print("============================")
-    print(" + 의 결과값 : \(calculateAdd)")
-    print(" - 의 결과값 : \(calculateSubstract)")
-    print(" / 의 결과값 : \(calculateDivide)")
-    print(" * 의 결과값 : \(calculateMutiply)")
-    print(" % 의 결과값 : \(calculateModulus)")
-    print()
-    
 }
 
 // 인스턴스 생성
-let calculator = Calculator(firstNumber: 34256, secondNumber: 453453)
-
-// 사칙연산 출력
-resultPrint(calculator)
-
-// 값 변경
-calculator.firstNumber = 2343536457
-calculator.secondNumber = 0
+let calculator = Calculator()
 
 // 사칙연산 출력
 resultPrint(calculator)
